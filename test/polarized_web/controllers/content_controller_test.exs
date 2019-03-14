a0c1b0dde0ed29eaf1ab404e7750773e8d991b7c
Michael Davis <mcd164@miami.edu>
2019-03-14 12:07:30 -0400

    Test getting the middle of a video

--- test/polarized_web/controllers/content_controller_test.exs
+++ test/polarized_web/controllers/content_controller_test.exs
@@ -9,33 +9,47 @@ defmodule PolarizedWeb.ContentControllerTest do
 
   setup :verify_on_exit!
 
-  setup do
-    embed = %Embed{
-      id: 113_654_156,
-      dest: Path.join([File.cwd!(), "data", "video.mp4"])
-    }
+  describe "getting a video" do
+    setup do
+      embed = %Embed{
+        id: 113_654_156,
+        dest: Path.join([File.cwd!(), "data", "video.mp4"])
+      }
 
-    [embed: embed]
-  end
+      [embed: embed]
+    end
 
-  # YARD this is awkward to fake http requests about videos
-  # it'd be nice to have some html-based thing like wallaby / hound for these
-  # things
-  test "getting a video", %{conn: conn} = c do
-    expect(@content_server, :get, fn _id -> c.embed end)
+    # This is awkward to fake http requests about videos it'd be nice to have
+    # some html-based thing like wallaby / hound for these things
+    test "successfully from the start", %{conn: conn} = c do
+      expect(@content_server, :get, fn _id -> c.embed end)
 
-    conn = get(conn, Routes.content_path(conn, :stream, c.embed))
+      conn = get(conn, Routes.content_path(conn, :stream, c.embed))
 
-    assert <<0, 0, 0, 28, 102, 116, 121>> <> _rest = conn.resp_body
-    assert {"content-type", "video/mp4"} in conn.resp_headers
-    assert conn.status == 206
-  end
+      assert <<0, 0, 0, 28, 102, 116, 121>> <> _rest = conn.resp_body
+      assert {"content-type", "video/mp4"} in conn.resp_headers
+      assert conn.status == 206
+    end
 
-  test "the video does not exist", %{conn: conn} = c do
-    expect(@content_server, :get, fn _id -> nil end)
+    test "successfully from the middle", %{conn: conn} = c do
+      expect(@content_server, :get, fn _id -> c.embed end)
 
-    conn = get(conn, Routes.content_path(conn, :stream, c.embed))
+      conn =
+        conn
+        |> Plug.Conn.put_req_header("range", "bytes=2000-")
+        |> get(Routes.content_path(conn, :stream, c.embed))
 
-    assert conn.status == 404
+      assert <<255, 255, 252, 23, 0, 0, 0>> <> _rest = conn.resp_body
+      assert {"content-type", "video/mp4"} in conn.resp_headers
+      assert conn.status == 206
+    end
+
+    test "when the video does not exist", %{conn: conn} = c do
+      expect(@content_server, :get, fn _id -> nil end)
+
+      conn = get(conn, Routes.content_path(conn, :stream, c.embed))
+
+      assert conn.status == 404
+    end
   end
 end
